378904042ed77693afc20d0ff0f3b71f6d63f0a3
Aman2 <aman-goyal>
2019-07-06 14:09:29 +0530

    Made Animation Better

--- collisions.py
+++ collisions.py
@@ -5,6 +5,12 @@ def reverse_vel(vel):
     vel *= -1
     return vel
 
+
 def exchange_vel(v1, m1, v2, m2):
+    '''
+    this function is calculating the new velocity of the block after collision,
+    based on law of conservation of momentum and kinetic energy
+    '''
     v1 = ((m1 - m2) / (m1 + m2)) * v1 + ((2 * m2) / (m1 + m2)) * v2
-    return v1
+
+    return v1  # returning new velocity after collision
--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import pygame
 from collisions import *
 pygame.init()
 
+s1, s2 = 100, 50  # block sides
+x1, y1 = 600, 250  # bigger block coords
+x2, y2 = 500, y1 + s1 - s2  # smaller block coords
 
-s1, s2 = 100, 50
-x1,y1 = 1000, 250
-x2, y2 = 500, y1+s1-s2
+power = int(input('enter: '))  # mass ratio
+v1 = (-5)  # initial velocity of block 1
 
-
-power = int(input('enter: '))
-step = 10*(power)
-v1 =  (-5/step)
-
-m1, m2 = 100**(power-1), 1
-v2 = 0
-
+m1, m2 = 20 * 100 ** (power - 1), 20  # mass of blocks
+v2 = 0  # initial velocity of block 2
 
 # temp_x1 = 0
 red = (255, 0, 0)
@@ -24,58 +20,65 @@ blue = (0, 0, 255)
 def message_to_print(msg, color):
     font = pygame.font.SysFont(None, 40)
     text = font.render(msg, True, color)
-    win.blit(text, [10,10])
-
-
-
+    win.blit(text, [10, 10])
 
 
 win = pygame.display.set_mode((1200, 500))
 win.fill((255, 255, 255))
 pygame.display.set_caption('simulation')
 
-Collisions = 0
+let_collision = 0
+Collisions = 0  # counting number of collisions
 run = True
 while run:
+    # pygame.time.delay(50)
     click_sound = pygame.mixer.Sound("rss/click.wav")
-    pygame.time.delay(10)
-    sound_collide, sound_reverse = True, True
-    for i in range(step):
+    sound = True
 
-        message_to_print('collision ' + str(Collisions), (0, 0, 0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+    message_to_print('collision ' + str(Collisions), (0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
+    # biger block
+    x1 += v1  # changing block coordinates according to velocity
+    if x1 >= s2 + 1:  # this prevents block 1 from moving out of window
+        t = x1
+        t2 = x2
+    else:
+        t2 = let_collision % 3
+    let_collision += 1
 
-        # biger block
-        x1 += v1
-        if x1>s2+2:
-            t=x1
+    if not x2 + s2 < x1 or x1 + s1 < x2:
+        '''
+        changing velocity after collision,
+        storing them in temp variable for each block,
+        then assiging new velocity
+        '''
+        v2_temp = exchange_vel(v2, m2, v1, m1)
+        v1_temp = exchange_vel(v1, m1, v2, m2)
+        if sound:
+            click_sound.play()
+            sound = False
 
-        if not x2 + s2 < x1 or x1 + s1 < x2:
-            v2_temp = exchange_vel(v2, m2, v1, m1)
-            v1_temp = exchange_vel(v1, m1, v2, m2)
-            if sound_collide:
-                click_sound.play()
-                sound = False
+        v2, v1 = v2_temp, v1_temp  # assigning new velocities
+        Collisions += 1
 
-            v2, v1 = v2_temp, v1_temp
-            Collisions += 1
+    # smaller Block
+    x2 += v2
+    if x2 <= 0:
+        '''
+        if block 1 touch left wall, its velocity reverses, 
+        '''
+        v2 = reverse_vel(v2)
+        if sound:
+            click_sound.play()
+            sound_reverse = False
+        Collisions += 1
+    pygame.draw.rect(win, blue, (t2, y2, s2, s2))
+    pygame.draw.rect(win, red, (t, y1, s1, s1))
 
-
-        # smaller Block
-        x2 += v2
-        if x2 <= 0:
-            v2 = reverse_vel(v2)
-            if sound_reverse:
-                click_sound.play()
-                sound_reverse = False
-            Collisions += 1
-        pygame.draw.rect(win, blue, (x2, y2, s2, s2))
-        pygame.draw.rect(win, red, (t, y1, s1, s1))
-
-        pygame.display.update()
-        win.fill((255, 255, 255))
+    pygame.display.update()
+    win.fill((255, 255, 255))
 
 pygame.quit()
